fbe72d315121833f471f665aa4fe774246813805
Jenelle Schilcher <jschilcher8@gmail.com>
2021-05-10 20:57:30 -0500

    Add arrays

--- tripgenerator.js
+++ tripgenerator.js
@@ -1,1 +1,11 @@
 "use strict";
+
+let destinations = ["Milwaukee", "Door County", "Wisconsin Dells", "Lake Geneva"];
+
+let resturants = ["Rock Bottom", "Oakfire", "Fireside", "Olive Garden"];
+
+let transportation = ["Bus", "Uber", "Lyft", "Rental"];
+
+let entertainment = ["Waterpark", "Horseback riding", "Zip line", "Arcade", "Summerfest", "Kite-surfing", "Shopping", "Boating"];
+
+
